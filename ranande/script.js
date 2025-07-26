b3a14a60534a3f18f8df8d75992820fc832f2ab6
Kamyar Khoshkalam <kamyarkhoshkalam@gmail.com>
2025-07-26 22:39:35 +0330

    section 3 of ranande complete

--- ranande/script.js
+++ ranande/script.js
@@ -1,3 +1,12 @@
+const range = document.querySelector("#range");
+const money = document.querySelector("#money");
+const time = document.querySelector("#time");
+
+range.addEventListener("input", (e) => {
+  time.textContent = range.value;
+  money.textContent = ((range.value / 4) * 21732000).toLocaleString();
+});
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
