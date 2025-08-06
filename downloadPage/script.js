808275fc1570d91e261ada5c2ff6f59324fd4d27
Kamyar Khoshkalam <kamyarkhoshkalam@gmail.com>
2025-08-07 03:04:22 +0330

    download page almost done

--- downloadPage/script.js
+++ downloadPage/script.js
@@ -4,3 +4,11 @@ const itemss = document.querySelector("#itemss");
 more.addEventListener("click", (e) => {
   itemss.classList.toggle("h-0");
 });
+
+const nav = document.querySelector("#nav");
+const hamburg = document.querySelector("#hamburg");
+
+hamburg.addEventListener("click", (e) => {
+  nav.classList.toggle("hidden");
+  nav.classList.toggle("flex");
+});
